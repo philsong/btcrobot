efadb38421dd08e9b990d649f4dec06bf66b3046
philsong <songbohr@163.com>
2014-03-07 17:33:32 +0800

    remove studygolang right, will optimize the file structure in later

--- websites/code/btcbot/src/process/process.go
+++ websites/code/btcbot/src/process/process.go
@@ -1,8 +1,8 @@
 // Copyright 2013 The StudyGolang Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-// http://studygolang.com
-// Author：polaris	studygolang@gmail.com
+// https://github.com/philsong
+// Author：Btcrobot
 
 package process
 
