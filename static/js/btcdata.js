2636472e48a94689f65f78293726f103ce838146
philsong <songbohr@163.com>
2014-03-24 16:54:29 +0800

    add  www-authenticate, default username/password is admin/123456

--- static/js/btcdata.js
+++ static/js/btcdata.js
@@ -1,7 +1,7 @@
-// Copyright 2014 The btcbot Authors. All rights reserved.
+// Copyright 2014 The btcrobot Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-// https://github.com/philsong/
+// https://github.com/philsong/btcrobot
 
 $(function() {
 	myTimer();
