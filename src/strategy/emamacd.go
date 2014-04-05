18770c2b4d5f38ac695716c5636d8778d878b9d5
phil <phil@phildeMacBook-Pro.local>
2014-04-05 11:28:43 +0800

    fix the macdema issue

--- src/strategy/emamacd.go
+++ src/strategy/emamacd.go
@@ -229,8 +229,7 @@ func (emamacdStrategy *EMAMACDStrategy) Perform(tradeAPI TradeAPI, records []Rec
 	//macd cross
 	if EMAdif[length-1] > 0 || emamacdStrategy.PrevEMATrade == "buy" {
 		logger.Debugln(Option["disable_trading"], emamacdStrategy.PrevMACDTrade)
-		if (Price[length-1] < emaLong[length-1]) &&
-			(MACDHistogram[length-2] > 0.000001 && MACDHistogram[length-1] < MACDsellThreshold) &&
+		if (MACDHistogram[length-2] > 0.000001 && MACDHistogram[length-1] < MACDsellThreshold) &&
 			emamacdStrategy.PrevMACDTrade != "sell" {
 			if Option["disable_trading"] != "1" && emamacdStrategy.PrevMACDTrade != "sell" {
 				emamacdStrategy.PrevMACDTrade = "sell"
