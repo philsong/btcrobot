da48128cf5ca0aeabdcd743df2ff72559b7d1e4d
PhilSong <78623269@qq.com>
2014-05-05 01:05:16 +0800

    release 0.35

--- src/strategy/HLCross.go
+++ src/strategy/HLCross.go
@@ -30,6 +30,7 @@ type HLCrossStrategy struct {
 	PrevBuyPirce     float64
 	PrevClosePrice   float64
 	PrevHighPrice    float64
+	PrevLowPrice     float64
 }
 
 func init() {
@@ -62,11 +63,13 @@ func (HLCross *HLCrossStrategy) Tick(records []Record) bool {
 	length := len(Price)
 
 	if HLCross.PrevClosePrice != records[length-1].Close ||
-		HLCross.PrevHighPrice != records[length-2].High {
+		HLCross.PrevHighPrice != records[length-2].High ||
+		HLCross.PrevLowPrice != records[length-2].Low {
 		HLCross.PrevClosePrice = records[length-1].Close
 		HLCross.PrevHighPrice = records[length-2].High
+		HLCross.PrevLowPrice = records[length-2].Low
 
-		logger.Infof("nowClose %0.02f prevHigh %0.02f\n", records[length-1].Close, records[length-2].High)
+		logger.Infof("nowClose %0.02f prevHigh %0.02f prevLow %0.02f\n", records[length-1].Close, records[length-2].High, records[length-2].Low)
 	}
 
 	//HLCross cross
