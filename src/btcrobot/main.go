79f158045cceaac42b63592a450180a2daeac1cb
PhilSong <78623269@qq.com>
2014-05-18 22:06:38 +0800

    add circulator order

--- src/btcrobot/main.go
+++ src/btcrobot/main.go
@@ -49,7 +49,7 @@ func main() {
 }
 
 func printBanner() {
-	version := "V0.37"
+	version := "V0.38"
 	fmt.Println("[ ---------------------------------------------------------->>> ")
 	fmt.Println(" BTC/LTC自动化算法交易引擎", version)
 	fmt.Println(" btcrobot is a Bitcoin, Litecoin and Altcoin trading bot written in golang")
