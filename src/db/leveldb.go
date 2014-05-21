161958922249c2d4d8d01ef1c89b5c21a4d0843f
Phil Song <songbohr@163.com>
2014-05-21 16:23:01 +0800

    save tx record in leveldb

--- src/db/leveldb.go
+++ src/db/leveldb.go
@@ -1,3 +1,20 @@
+/*
+  btcrobot is a Bitcoin, Litecoin and Altcoin trading bot written in golang,
+  it features multiple trading methods using technical analysis.
+
+  Disclaimer:
+
+  USE AT YOUR OWN RISK!
+
+  The author of this project is NOT responsible for any damage or loss caused
+  by this software. There can be bugs and the bot may not Tick as expected
+  or specified. Please consider testing it first with paper trading /
+  backtesting on historical data. Also look at the code to see what how
+  it's working.
+
+  Weibo:http://weibo.com/bocaicfa
+*/
+
 package db
 
 import (
