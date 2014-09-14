31e08faf00da5c1a25ff0aaac247c5042ad2288e
PhilSong <78623269@qq.com>
2014-09-14 20:45:13 +0800

    change ok from REST to WEBsocke

--- static/js/okcoin.js
+++ static/js/okcoin.js
@@ -3,161 +3,197 @@
 // license that can be found in the LICENSE file.
 // https://github.com/philsong/btcrobot
 
-$(function() {
-	myTimer();
-});
+var wsUri = "wss://real.okcoin.cn:10440/websocket/okcoinapi";
+var output;
 
-var myVar = setInterval(function() {
-	myTimer()
-}, 10000);
-
-function myStopFunction() {
-	clearInterval(myVar);
+function init() {
+	output = document.getElementById("output");
+	testWebSocket();
 }
 
-function myStartFunction() {
-	clearInterval(myVar);
-	myVar = setInterval(function() {
-		myTimer()
-	}, 10000);
+function testWebSocket() {
+	websocket = new WebSocket(wsUri);
+	websocket.onopen = function(evt) {
+		onOpen(evt)
+	};
+	websocket.onclose = function(evt) {
+		onClose(evt)
+	};
+	websocket.onmessage = function(evt) {
+		onMessage(evt);
+		websocket.close();
+	};
+	websocket.onerror = function(evt) {
+		onError(evt)
+		
+	};
 }
 
-function myTimer() {
-	$.ajaxSetup({
-		// Disable caching of AJAX responses
-		cache: false
+function onOpen(evt) {
+	//writeToScreen("CONNECTED");
+	doSend("{'event':'addChannel','channel':'ok_btccny_kline_1hour'}");
+}
+
+function onClose(evt) {
+	//writeToScreen("DISCONNECTED");
+}
+
+function onMessage(evt) {
+	//writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data + '</span>');
+
+	// split the data set into ohlc and volume
+	var datas = [];
+
+	// Split the lines
+	//console.log(evt.data);
+	var lines = JSON.parse(evt.data);
+	//var lines = evt.data.data;
+	console.log(lines[0].data);
+
+	//writeToScreen('<span style="color: blue;">RESPONSE: ' + lines[0].data+ '</span>');
+	//lines = lines.slice(-290) //okcoin total:1440, but huobi only 4hour50minutes=290minutes
+
+	// Iterate over the lines and add categories or series
+	$.each(lines[0].data, function(lineNo, line) {
+		//console.log(line[0])
+		var d = new Date();
+		d.setTime(line[0])
+
+		datas.push([
+			d.getTime(),
+			parseFloat(line[4])
+		]);
 	});
 
-	$.get('https://www.okcoin.com/kline/period.do?step=60&symbol=okcoinbtccny', function(data) {
-		// split the data set into ohlc and volume
-		var datas = [];
-
-		// Split the lines
-		var lines = JSON.parse(data);
-		lines = lines.slice(-290) //okcoin total:1440, but huobi only 4hour50minutes=290minutes
-
-		// Iterate over the lines and add categories or series
-		$.each(lines, function(lineNo, line) {
-			//console.log(line[0])
-			var d = new Date();
-			d.setTime(1000*line[0])
-			
-			datas.push([
-				d.getTime(),
-				parseFloat(line[6])
-			]);	
-		});
-
-		// Create the chart
-		$('#indictorcontainer').highcharts('StockChart', {
+	// Create the chart
+	$('#indictorcontainer').highcharts('StockChart', {
 
 
+		title: {
+			text: 'OKcoin趋势指标'
+		},
+		subtitle: {
+			text: '时间序列价格分析系统'
+		},
+		xAxis: {
+			type: 'datetime'
+		},
+
+		yAxis: [{
 			title: {
-				text: 'OKcoin趋势指标'
+				text: '价格'
 			},
-			subtitle: {
-				text: '时间序列价格分析系统'
-			},
-			xAxis: {
-				type: 'datetime'
-			},
-
-			yAxis: [{
-				title: {
-					text: '价格'
-				},
-				height: 200,
-				plotLines: [{
-					value: 0,
-					width: 1,
-					color: '#808080'
-				}]
-			}, {
-				title: {
-					text: 'MACD'
-				},
-				top: 300,
-				height: 100,
-				offset: 0,
-				lineWidth: 0.1
-			}],
-
-			tooltip: {
-				crosshairs: true,
-				shared: true
-			},
-
-			rangeSelector: {
-				selected: 1
-			},
-
-			legend: {
-				enabled: true,
-				layout: 'vertical',
-				align: 'right',
-				verticalAlign: 'middle',
-				borderWidth: 0
-			},
-
-			plotOptions: {
-				series: {
-					marker: {
-						enabled: false,
-					}
-				}
-			},
-
-
-			series: [{
-				name: '价格',
-				type: 'line',
-				id: 'primary',
-				data: datas
-			}, {
-				name: 'MACD',
-				linkedTo: 'primary',
-				yAxis: 1,
-				showInLegend: true,
-				type: 'trendline',
-				algorithm: 'MACD'
-
-			}, {
-				name: 'Signal line信号线',
-				linkedTo: 'primary',
-				yAxis: 1,
-				showInLegend: true,
-				type: 'trendline',
-				algorithm: 'signalLine'
-
-			}, {
-				name: 'Histogram柱状图',
-				linkedTo: 'primary',
-				yAxis: 1,
-				showInLegend: true,
-				type: 'histogram'
-
-			}, {
-				name: '10-EMA',
-				linkedTo: 'primary',
-				showInLegend: true,
-				type: 'trendline',
-				algorithm: 'EMA',
-				periods: 10
-			}, {
-				name: '21-EMA',
-				linkedTo: 'primary',
-				showInLegend: true,
-				type: 'trendline',
-				algorithm: 'EMA',
-				periods: 21
-			}, {
-				name: 'Linear Trendline趋势线',
-				linkedTo: 'primary',
-				showInLegend: true,
-				enableMouseTracking: false,
-				type: 'trendline',
-				algorithm: 'linear'
+			height: 200,
+			plotLines: [{
+				value: 0,
+				width: 1,
+				color: '#808080'
 			}]
-		});
-	}, "text");
+		}, {
+			title: {
+				text: 'MACD'
+			},
+			top: 300,
+			height: 100,
+			offset: 0,
+			lineWidth: 0.1
+		}],
+
+		tooltip: {
+			crosshairs: true,
+			shared: true
+		},
+
+		rangeSelector: {
+			selected: 1
+		},
+
+		legend: {
+			enabled: true,
+			layout: 'vertical',
+			align: 'right',
+			verticalAlign: 'middle',
+			borderWidth: 0
+		},
+
+		plotOptions: {
+			series: {
+				marker: {
+					enabled: false,
+				}
+			}
+		},
+
+
+		series: [{
+			name: '价格',
+			type: 'line',
+			id: 'primary',
+			data: datas
+		}, {
+			name: 'MACD',
+			linkedTo: 'primary',
+			yAxis: 1,
+			showInLegend: true,
+			type: 'trendline',
+			algorithm: 'MACD'
+
+		}, {
+			name: 'Signal line信号线',
+			linkedTo: 'primary',
+			yAxis: 1,
+			showInLegend: true,
+			type: 'trendline',
+			algorithm: 'signalLine'
+
+		}, {
+			name: 'Histogram柱状图',
+			linkedTo: 'primary',
+			yAxis: 1,
+			showInLegend: true,
+			type: 'histogram'
+
+		}, {
+			name: '10-EMA',
+			linkedTo: 'primary',
+			showInLegend: true,
+			type: 'trendline',
+			algorithm: 'EMA',
+			periods: 10
+		}, {
+			name: '21-EMA',
+			linkedTo: 'primary',
+			showInLegend: true,
+			type: 'trendline',
+			algorithm: 'EMA',
+			periods: 21
+		}, {
+			name: 'Linear Trendline趋势线',
+			linkedTo: 'primary',
+			showInLegend: true,
+			enableMouseTracking: false,
+			type: 'trendline',
+			algorithm: 'linear'
+		}]
+	});
+
+	//websocket.close();
 }
+
+function onError(evt) {
+	writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+}
+
+function doSend(message) {
+	//writeToScreen("SENT: " + message);
+	websocket.send(message);
+}
+
+function writeToScreen(message) {
+	var pre = document.createElement("p");
+	pre.style.wordWrap = "break-word";
+	pre.innerHTML = message;
+	output.appendChild(pre);
+}
+window.addEventListener("load", init, false);
+
